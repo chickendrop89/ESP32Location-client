1b1db1e8e0a8ece1bb9aa23036173e6a129b2430
chickendrop89 <chickendrop89@gmail.com>
2024-11-10 13:29:58 +0100

    main: add copyright notice
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- main.ino
+++ main.ino
@@ -1,3 +1,18 @@
+/*
+  A small project to determine location using beaconDB or similar API
+  Copyright (C) 2024  chickendrop89
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+*/
+
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 
